a805546f8c498875210c2fdf60ddd569879af502
ArtiTemkin <a.zymarev@icloud.com>
2023-01-31 17:57:59 +0200

    ДЗ после проверки

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 const inputValue = document.querySelector('#searchBox');
 
 import { searchImages } from './js/searchImages';
+import { generateGallery } from './js/generateGallery';
 
 const buttonLoad = document.querySelector('.load-more');
 const form = document.querySelector('#search-form');
@@ -15,70 +16,45 @@ var lightbox = new SimpleLightbox('.gallery a');
 let currentPage = 1;
 
 form.addEventListener('submit', search);
+buttonLoad.addEventListener('click', loadMore);
 
 function search(event) {
   event.preventDefault();
   currentPage = 1;
   searchImages(inputValue.value, currentPage)
     .then(response => {
-      Notiflix.Notify.info(
-        'Hooray! We found totalHits ' + response.data.totalHits + ' images.'
-      );
-      return response;
+      if (response.data.totalHits === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else {
+        Notiflix.Notify.info(
+          'Hooray! We found totalHits ' + response.data.totalHits + ' images.'
+        );
+      }
+      if (Number(response.data.totalHits) > 40) {
+        document.getElementById('load-more').style.visibility = 'visible';
+      }
+      return response.data;
     })
-    .then(response => processResponse(response.data))
-    .then(galleryItems => {
-      gallery.innerHTML = galleryItems;
+    .then(data => {
+      gallery.innerHTML = generateGallery(data.hits);
       lightbox.refresh();
+    })
+    .catch(function (error) {
+      console.log(error);
     });
-
-  function loadMore() {
-    searchImages(inputValue.value, ++currentPage)
-      .then(response => processResponse(response.data))
-      .then(galleryItems => {
-        gallery.insertAdjacentHTML('beforeend', galleryItems);
-        lightbox.refresh();
-      });
-  }
-
-  buttonLoad.addEventListener('click', loadMore);
-
-  function processResponse(data) {
-    if (data.totalHits === 0) {
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-      return '';
-    }
-    let sum = (currentPage - 1) * 40 + data.hits.length;
-
-    document.getElementById('load-more').style.visibility =
-      Number(sum) < Number(data.totalHits) ? 'visible' : 'hidden';
-
-    return generateGallery(data.hits);
-  }
 }
 
-function generateGallery(items) {
-  return items
-    .map(
-      item => `<a href='${item.largeImageURL}'><div class="photo-card">
-               <img src='${item.webformatURL}' alt="" loading="lazy" />
-               <div class="info">
-                 <p class="info-item">
-                   <b>Likes ${item.likes}</b>
-                 </p>
-                 <p class="info-item">
-                   <b>Views ${item.views}</b>
-                 </p>
-                 <p class="info-item">
-                   <b>Comments ${item.comments}</b>
-                 </p>
-                 <p class="info-item">
-                   <b>Downloads ${item.downloads}</b>
-                 </p>
-               </div>
-             </div></a>`
-    )
-    .join('');
+function loadMore() {
+  searchImages(inputValue.value, ++currentPage).then(response => {
+    if (Number(response.data.hits.length) < 40) {
+      document.getElementById('load-more').style.visibility = 'hidden';
+    }
+    gallery.insertAdjacentHTML(
+      'beforeend',
+      generateGallery(response.data.hits)
+    );
+    lightbox.refresh();
+  });
 }
